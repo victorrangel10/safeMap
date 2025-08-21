61735d8a54f824dc99aad18003092f5698e4a529
Victor Rangel <victoraugusto.es@gmail.com>
2025-08-21 19:32:42 -0300

    initializing project

---
+++ src/main/java/br/com/safemap/safemap/SafemapApplication.java
@@ -0,0 +1,13 @@
+package br.com.safemap.safemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafemapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafemapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/safemap/safemap/SafemapApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.safemap.safemap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafemapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
